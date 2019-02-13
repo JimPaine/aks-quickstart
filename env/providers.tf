43a2aeeffe27b214bc825edaf693253a03f9bd1e
Jim Paine <jim@jimpaine.io>
2019-02-13 15:13:27 +0000

    removed old vars

--- env/providers.tf
+++ env/providers.tf
@@ -17,3 +17,12 @@ provider "kubernetes" {
   client_key             = "${base64decode(azurerm_kubernetes_cluster.demo.kube_config.0.client_key)}"
   cluster_ca_certificate = "${base64decode(azurerm_kubernetes_cluster.demo.kube_config.0.cluster_ca_certificate)}"
 }
+
+provider "helm" {  
+  kubernetes {
+    host                   = "${azurerm_kubernetes_cluster.demo.kube_config.0.host}"
+    client_certificate     = "${base64decode(azurerm_kubernetes_cluster.demo.kube_config.0.client_certificate)}"
+    client_key             = "${base64decode(azurerm_kubernetes_cluster.demo.kube_config.0.client_key)}"
+    cluster_ca_certificate = "${base64decode(azurerm_kubernetes_cluster.demo.kube_config.0.cluster_ca_certificate)}"
+  }
+}
--- env/variables.tf
+++ env/variables.tf
@@ -3,5 +3,3 @@ variable "subscription_id" {}
 variable "client_id" {}
 variable "client_secret" {}
 variable "tenant_id" {}
-variable "public_key" {}
-variable "private_key" {}
