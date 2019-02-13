a9fd054603faeb59d69e0ba976cdd757be29e622
Jim Paine <jim@jimpaine.io>
2019-02-13 15:28:15 +0000

    removed old SP

--- env/aks.tf
+++ env/aks.tf
@@ -1,27 +1,3 @@
-resource "azurerm_azuread_application" "demo" {
-  name                       = "${var.resource_name}${random_id.demo.dec}"
-  homepage                   = "http://homepage${random_id.demo.dec}"
-  identifier_uris            = ["http://uri${random_id.demo.dec}"]
-  reply_urls                 = ["http://replyurl${random_id.demo.dec}"]
-  available_to_other_tenants = false
-  oauth2_allow_implicit_flow = true
-}
-
-resource "azurerm_azuread_service_principal" "demo" {
-  application_id = "${azurerm_azuread_application.demo.application_id}"
-}
-
-resource "random_string" "demo" {
-  length  = "32"
-  special = true
-}
-
-resource "azurerm_azuread_service_principal_password" "demo" {
-  service_principal_id = "${azurerm_azuread_service_principal.demo.id}"
-  value                = "${random_string.demo.result}"
-  end_date             = "2020-01-01T01:02:03Z"
-}
-
 resource "azurerm_kubernetes_cluster" "demo" {
   name                = "${var.resource_name}${random_id.demo.dec}"
   location            = "${azurerm_resource_group.demo.location}"
