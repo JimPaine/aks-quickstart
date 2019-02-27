102a1433dadd57a13b30f111369b9ead6a82ce0d
Jim Paine <jim@jimpaine.io>
2019-02-27 22:13:26 +0000

    added backend

---
+++ env/backend.tf
@@ -0,0 +1,10 @@
+terraform {
+  backend "azurerm" {
+    #example properties
+    #resource_group_name  = ""
+    #storage_account_name = ""
+    #container_name       = ""
+    #key                  = ""
+    #access_key           = ""
+  }
+}
