c3082d32abbfd06868b574dc469f43f3d851d036
Jim Paine <jim@jimpaine.io>
2019-02-28 14:44:27 +0000

    removed nginx

--- env/helm.tf
+++ env/helm.tf
@@ -13,18 +13,3 @@ locals {
     "service.beta.kubernetes.io/azure-load-balancer-internal" = "true"
   }
 }
-
-resource "helm_release" "nginxingress" {
-    name      = "nginx-ingress"
-    chart     = "stable/nginx-ingress"
-
-    set_string {
-        name = "controller.service.annotations"
-        value = "${ jsonencode(local.annotations) }"
-    }
-
-    set_string {
-        name = "controller.service.loadBalancerIP"
-        value = "10.1.0.254"
-    }
-}
