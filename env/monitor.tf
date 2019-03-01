1fc6a4d688de39cadf57d712585598187033e60e
Jim Paine <jim@jimpaine.io>
2019-03-01 09:42:21 +0000

    change sku

--- env/monitor.tf
+++ env/monitor.tf
@@ -2,6 +2,6 @@ resource "azurerm_log_analytics_workspace" "demo" {
   name                = "${var.resource_name}${random_id.demo.dec}logs"
   location            = "${azurerm_resource_group.demo.location}"
   resource_group_name = "${azurerm_resource_group.demo.name}"
-  sku                 = "PerGB2018"
+  sku                 = "PerNode"
   retention_in_days   = 30
 }
