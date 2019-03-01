319cfaa60a30c9991310755b8dbc6cf62c27cd10
Jim Paine <jim@jimpaine.io>
2019-03-01 10:33:25 +0000

    added container insights

--- env/monitor.tf
+++ env/monitor.tf
@@ -5,3 +5,16 @@ resource "azurerm_log_analytics_workspace" "demo" {
   sku                 = "PerNode"
   retention_in_days   = 30
 }
+
+resource "azurerm_log_analytics_solution" "demo" {
+  solution_name         = "ContainerInsights"
+  location              = "${azurerm_resource_group.demo.location}"
+  resource_group_name   = "${azurerm_resource_group.demo.name}"
+  workspace_resource_id = "${azurerm_log_analytics_workspace.demo.id}"
+  workspace_name        = "${azurerm_log_analytics_workspace.demo.name}"
+
+  plan {
+    publisher = "Microsoft"
+    product   = "OMSGallery/ContainerInsights"
+  }
+}
