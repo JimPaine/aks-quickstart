7b1e34ed1f3001b5db438dbb81804d267fad121a
Jim Paine <jim@jimpaine.io>
2019-03-01 13:41:27 +0000

    added nginx

--- env/helm.tf
+++ env/helm.tf
@@ -7,3 +7,18 @@ resource "helm_release" "grafana" {
     name      = "grafana"
     chart     = "stable/grafana"
 }
+
+resource "helm_release" "nginx" {
+    name      = "nginx"
+    chart     = "stable/nginx"
+
+    values = [<<VALUES
+        controller:
+          service:
+            type: LoadBalancer
+            loadBalancerIP: 10.240.0.25
+            annotations: 
+              service.beta.kubernetes.io/azure-load-balancer-internal: "true"
+    VALUES
+    ]
+}
