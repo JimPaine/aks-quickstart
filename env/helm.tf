da4cc7e69b8cc6009c5141fdfb31668e3f147141
Jim Paine <jim@jimpaine.io>
2019-03-14 15:05:05 +0000

    add traefik

--- env/helm.tf
+++ env/helm.tf
@@ -8,5 +8,22 @@ resource "helm_release" "grafana" {
     chart     = "stable/grafana"
 }
 
+resource "helm_release" "traefik" {
+    name      = "traefik"
+    chart     = "stable/traefik"
 
+    namespace = "dev"
+
+    set {
+        name = "loadBalancerIP"
+        value = "10.1.0.254"
+    }
+
+    values = [<<VALUES
+        service:
+          annotations:
+            service.beta.kubernetes.io/azure-load-balancer-internal: "true"
+    VALUES
+    ]
+}
 
