4e4abee369c07e806ed042b3974cf1ec761d216e
Jim Paine <jim@jimpaine.io>
2019-03-14 16:00:41 +0000

    rbac and sa for traefik

--- env/traefik.tf
+++ env/traefik.tf
@@ -38,7 +38,7 @@ resource "kubernetes_cluster_role" "traefik" {
 
 resource "kubernetes_cluster_role_binding" "traefik" {
     metadata {
-        name = "clustertiller-binding"
+        name = "traefik-controller-binding"
     }
     role_ref {
         api_group = "rbac.authorization.k8s.io"
