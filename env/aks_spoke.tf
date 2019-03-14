e4730a125e1c841099f48ec0b521e6b33f492030
Jim Paine <jim@jimpaine.io>
2019-03-14 14:27:58 +0000

    added route table

--- env/aks_spoke.tf
+++ env/aks_spoke.tf
@@ -12,3 +12,21 @@ resource "azurerm_subnet" "aks" {
   address_prefix       = "10.1.0.0/24"
   virtual_network_name = "${azurerm_virtual_network.aks.name}"
 }
+
+resource "azurerm_route_table" "demo" {
+  name                          = "hubrouter"
+  location                      = "${azurerm_resource_group.demo.location}"
+  resource_group_name           = "${azurerm_resource_group.demo.name}"
+  disable_bgp_route_propagation = false
+
+  route {
+    name           = "route1"
+    address_prefix = "10.2.0.0/16"
+    next_hop_type  = "vnetlocal"
+  }
+}
+
+resource "azurerm_subnet_route_table_association" "demo" {
+  subnet_id      = "${azurerm_subnet.aks.id}"
+  route_table_id = "${azurerm_route_table.demo.id}"
+}
