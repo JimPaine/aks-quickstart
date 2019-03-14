3ca16e1cc264c68b783bc67a6e880c7bbeae7525
Jim Paine <jim@jimpaine.io>
2019-03-14 16:09:15 +0000

    helm set traefik sa

--- env/traefik.tf
+++ env/traefik.tf
@@ -13,42 +13,8 @@ resource "helm_release" "traefik" {
         service:
           annotations:
             service.beta.kubernetes.io/azure-load-balancer-internal: "true"
+        rbac:
+          enabled: true
     VALUES
     ]
-}
-
-resource "kubernetes_service_account" "traefik" {
-  metadata {
-    name = "traefik"
-    namespace = "dev"
-  }
-}
-
-resource "kubernetes_cluster_role" "traefik" {
-    metadata {
-        name = "traefik-controller"
-    }
-
-    rule {
-        api_groups = ["extensions"]
-        resources  = ["ingresses"]
-        verbs      = ["get", "list", "watch"]
-    }
-}
-
-resource "kubernetes_cluster_role_binding" "traefik" {
-    metadata {
-        name = "traefik-controller-binding"
-    }
-    role_ref {
-        api_group = "rbac.authorization.k8s.io"
-        kind = "ClusterRole"
-        name = "traefik-controller"
-    }
-    subject {
-        kind = "ServiceAccount"
-        name = "traefik"
-        namespace = "dev"
-        api_group = ""
-    }
 }
