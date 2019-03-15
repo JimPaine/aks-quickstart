f0eb302dae06e718a167b07399089092901bfbc7
Jim Paine <jim@jimpaine.io>
2019-03-15 15:38:40 +0000

    changed cidr

--- env/aks.tf
+++ env/aks.tf
@@ -36,7 +36,7 @@ resource "azurerm_kubernetes_cluster" "demo" {
   network_profile {
     network_plugin = "azure"
     dns_service_ip = "10.2.1.253"
-    service_cidr = "10.2.1.1/24"
+    service_cidr = "10.2.2.0/24"
     docker_bridge_cidr = "172.17.0.1/16"
   }
 
