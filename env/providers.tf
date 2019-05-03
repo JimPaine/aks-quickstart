2de5f63eeda51ed1771e0887df0be72474743ee8
Jim Paine <jim@jimpaine.io>
2019-05-03 11:03:23 +0100

    updated azure provider ver

--- env/providers.tf
+++ env/providers.tf
@@ -1,6 +1,6 @@
 provider "azurerm" {
   subscription_id = "${var.subscription_id}"
-  version         = "~> 1.23"
+  version         = "~> 1.27"
   client_id       = "${var.client_id}"
   client_secret   = "${var.client_secret}"
   tenant_id       = "${var.tenant_id}"
