a7b46939ff621e87b2a3c5b048ddf8b7899e0740
Jim Paine <jim@jimpaine.io>
2019-05-11 16:44:46 +0100

    switch tiller sa

--- env/providers.tf
+++ env/providers.tf
@@ -42,9 +42,9 @@ provider "tls" {
 
 provider "helm" {
   version = "~> 0.9"
-  service_account = "tiller"
+  service_account = "clustertiller"
   namespace = "dev" 
-  
+
     kubernetes {
       host                   = "${azurerm_kubernetes_cluster.aks.kube_config.0.host}"
       client_certificate     = "${base64decode(azurerm_kubernetes_cluster.aks.kube_config.0.client_certificate)}"
