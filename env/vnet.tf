9d1f1916fa09cced3604aed27896038c44ca7dfd
Jim Paine <jim@jimpaine.io>
2019-06-18 01:51:23 +0100

    added delegation

--- env/vnet.tf
+++ env/vnet.tf
@@ -17,4 +17,11 @@ resource "azurerm_subnet" "virtual" {
   resource_group_name  = "${azurerm_resource_group.aks.name}"
   address_prefix       = "10.2.3.0/24"
   virtual_network_name = "${azurerm_virtual_network.aks.name}"
+
+  delegation {
+    name = "aciDelegation"
+    service_delegation {
+      name    = "Microsoft.ContainerInstance/containerGroups"
+      actions = ["Microsoft.Network/virtualNetworks/subnets/action"]
+    }
 }
